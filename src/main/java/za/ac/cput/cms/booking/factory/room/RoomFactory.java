1466b22aa9cd9fa6897c0e27dfbb2a7a0a2c3abd
Justin <justinjenecke@gmail.com>
2021-09-14 16:15:05 +0200

    populates RoomFactory with code to create instances of Room.class

--- src/main/java/za/ac/cput/cms/booking/factory/room/RoomFactory.java
+++ src/main/java/za/ac/cput/cms/booking/factory/room/RoomFactory.java
@@ -1,5 +1,12 @@
 package za.ac.cput.cms.booking.factory.room;
 
+import za.ac.cput.cms.booking.entity.room.BookingCalendar;
+import za.ac.cput.cms.booking.entity.room.CalendarMonth;
+import za.ac.cput.cms.booking.entity.room.Room;
+import za.ac.cput.cms.booking.entity.room.RoomDetails;
+
+import java.util.ArrayList;
+
 /**
  * Author: Justin Jenecke 215163052
  * Factory class for creating instances of Room.Class
@@ -7,4 +14,24 @@ package za.ac.cput.cms.booking.factory.room;
  * */
 
 public class RoomFactory {
+
+    public static Room buildRoom(int id,
+                                 int bedType,
+                                 int roomType,
+                                 RoomDetails roomDetails,
+                                 BookingCalendar bookingCalendar,
+                                 int cost) {
+
+        Room room = new Room.Builder()
+                .setRoomNo(101)
+                .setBedType(2)
+                .setRoomType(5)
+                .setRoomDetails(roomDetails)
+                .setBookingCalendar(bookingCalendar)
+                .setCost(cost)
+                .build();
+
+        return room;
+    }
+
 }

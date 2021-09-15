74da7e295765ffce863ab39c318c1fdc99ec759f
Justin <justinjenecke@gmail.com>
2021-09-15 18:23:57 +0200

    adds calendarMonthservice class to project w/CRUD functions

--- src/main/java/za/ac/cput/cms/booking/service/room/impl/CalendarMonthService.java
+++ src/main/java/za/ac/cput/cms/booking/service/room/impl/CalendarMonthService.java
@@ -1,10 +1,41 @@
 package za.ac.cput.cms.booking.service.room.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.cms.booking.entity.room.CalendarMonth;
+import za.ac.cput.cms.booking.repository.room.ICalendarMonthRepository;
+import za.ac.cput.cms.booking.service.room.ICalendarMonthService;
+
 /**
  * Author: Justin Jenecke 215163052
  * Service class exposing repository methods for CalendarMonth.Class
  * Created: 15/09/2021
  * */
 
-public class CalendarMonthService {
+@Service
+public class CalendarMonthService implements ICalendarMonthService {
+
+    @Autowired
+    private ICalendarMonthRepository calendarMonthRepository;
+
+    @Override
+    public CalendarMonth create(CalendarMonth calendarMonth) {
+        return this.calendarMonthRepository.save(calendarMonth);
+    }
+
+    @Override
+    public CalendarMonth read(String s) {
+        return this.calendarMonthRepository.findById(s).orElseGet(null);
+    }
+
+    @Override
+    public CalendarMonth update(CalendarMonth calendarMonth) {
+        return this.calendarMonthRepository.save(calendarMonth);
+    }
+
+    @Override
+    public boolean delete(String s) {
+        this.calendarMonthRepository.deleteById(s);
+        return !this.calendarMonthRepository.existsById(s);
+    }
 }

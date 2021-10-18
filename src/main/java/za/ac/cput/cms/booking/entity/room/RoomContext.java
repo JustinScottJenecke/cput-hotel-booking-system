3dfb699c8359f2e00c30955d4895be205d926cbb
Justin <justinjenecke@gmail.com>
2021-10-18 22:44:33 +0200

    Creates RoomContext class for holding all Room related entities in single RequestBody object

---
+++ src/main/java/za/ac/cput/cms/booking/entity/room/RoomContext.java
@@ -0,0 +1,12 @@
+package za.ac.cput.cms.booking.entity.room;
+
+/**
+ * Author: Justin Scott Jenecke
+ * Description: Room Context class for holding all Room related entities in single RequestBody object
+ * Date: 18/10/2021
+ * */
+
+public class RoomContext {
+
+    private Room room;
+}

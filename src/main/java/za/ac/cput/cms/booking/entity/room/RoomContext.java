829a6818a619d1726fd2129ff48ab850162bd24e
Justin <justinjenecke@gmail.com>
2021-10-19 11:25:04 +0200

    adds endpoint to RoomController for creating a RoomContext, and adds placeholder functionality while waiting for team to complete their entities for integration

--- src/main/java/za/ac/cput/cms/booking/entity/room/RoomContext.java
+++ src/main/java/za/ac/cput/cms/booking/entity/room/RoomContext.java
@@ -9,4 +9,56 @@ package za.ac.cput.cms.booking.entity.room;
 public class RoomContext {
 
     private Room room;
+    int roomDetails;
+    int bedType;
+    int roomType;
+
+    public RoomContext(Room room, int roomDetails, int bedType, int roomType) {
+        this.room = room;
+        this.roomDetails = roomDetails;
+        this.bedType = bedType;
+        this.roomType = roomType;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public int getRoomDetails() {
+        return roomDetails;
+    }
+
+    public void setRoomDetails(int roomDetails) {
+        this.roomDetails = roomDetails;
+    }
+
+    public int getBedType() {
+        return bedType;
+    }
+
+    public void setBedType(int bedType) {
+        this.bedType = bedType;
+    }
+
+    public int getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(int roomType) {
+        this.roomType = roomType;
+    }
+
+    @Override
+    public String toString() {
+        return "RoomContext{" +
+                "room=" + room +
+                ", roomDetails=" + roomDetails +
+                ", bedType=" + bedType +
+                ", roomType=" + roomType +
+                '}';
+    }
 }

37fd98220ee947fe98418db51e84a42db96b7d62
Justin <justinjenecke@gmail.com>
2021-10-19 11:01:36 +0200

    Creates main Spring-Boot method in Application.java, adds configurations for H2 db integration, and updates H2 dependancy in pom to move H2 db from testing contect to runtime context

---
+++ src/main/java/za/ac/cput/cms/booking/Application.java
@@ -0,0 +1,13 @@
+package za.ac.cput.cms.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- src/main/java/za/ac/cput/cms/booking/controller/views/loginController.java
+++ src/main/java/za/ac/cput/cms/booking/controller/views/loginController.java
@@ -1,4 +1,22 @@
 package za.ac.cput.cms.booking.controller.views;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author: Justin Scott Jenecke 215163052
+ * REST Controller for generic views and testing
+ * Created: 18/10/2021
+ * */
+
+@RestController
+@RequestMapping("/za.ac.cput.hotelbookingservice/api")
 public class loginController {
+
+    @RequestMapping("")
+    public String landing() {
+        return "/za.ac.cput.hotelbookingservice/api - booking - room";
+    }
+
+
 }
